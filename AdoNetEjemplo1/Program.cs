8d6d862855f98c5b667b401aa7bf368e215d0b2b
Javier Lete <javier@eskura21.com>
2021-09-03 13:16:45 +0200

    SELECT básico

--- AdoNetEjemplo1/Program.cs
+++ AdoNetEjemplo1/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+using System.Data.SqlClient;
+
 namespace AdoNetEjemplo1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IDbConnection con = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=mf0966;Integrated Security=True"); //@"Server=localhost\SQLEXPRESS;Database=mf0966;Trusted_Connection=True;");
+            IDbCommand com = con.CreateCommand();
+            com.CommandText = "SELECT * FROM Clientes";
+            con.Open();
+            IDataReader dr = com.ExecuteReader();
+            
+            while(dr.Read())
+            {
+                Console.WriteLine($"{dr["ClienteId"]}, {dr["Nombre"]}, {dr["Apellidos"]}, {dr["FechaNacimiento"]}");
+            }
         }
     }
 }
